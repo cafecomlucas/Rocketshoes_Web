b27530336551702ebb9b982778c0a062c927542f
Lucas Soares <cafecomlucas@gmail.com>
2020-01-10 19:23:42 -0300

    Cart | Ajusta estrutura de acordo com os erros do console

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -12,42 +12,46 @@ export default function Cart() {
       <ContainerTable>
         <ProductTable>
           <thead>
-            <th />
-            <th>PRODUTO</th>
-            <th>QTD</th>
-            <th>SUBTOTAL</th>
-            <th />
+            <tr>
+              <th />
+              <th>PRODUTO</th>
+              <th>QTD</th>
+              <th>SUBTOTAL</th>
+              <th />
+            </tr>
           </thead>
           <tbody>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-caminhada-detalhes-em-couro-masculino/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tênis maneiro</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
+            <tr>
+              <td>
+                <img
+                  src="https://static.netshoes.com.br/produtos/tenis-caminhada-detalhes-em-couro-masculino/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+                  alt="Tênis"
+                />
+              </td>
+              <td>
+                <strong>Tênis maneiro</strong>
+                <span>R$129,90</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readyonly="true" />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readyOnly value={2} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
+              </td>
+            </tr>
           </tbody>
         </ProductTable>
       </ContainerTable>
