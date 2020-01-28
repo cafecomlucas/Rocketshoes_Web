38f49adc926ae6267cacfa74cea1503087852efe
Lucas Soares <cafecomlucas@gmail.com>
2020-01-27 23:35:56 -0300

    Configura pro deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rocketshoesapp.herokuapp.com:3333',
+  baseURL: 'https://rocketshoesapp.herokuapp.com',
 });
 
 export default api;
